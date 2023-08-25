89be987bf57a060f51210416e9e8b5224ca57551
LucasIbanez13 <lucaas00129@gmail.com>
2023-08-25 15:52:27 -0300

    agrego proyecto

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -9,6 +9,8 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
+		const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 		return res.render('products',{
 			products,
 			toThousand
@@ -32,21 +34,51 @@ const controller = {
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
+		const {name,price,description,discount,category} = req.body;
+		const product = {
+			id : products[products.length -1].id + 1,
+			name : name.trim(),
+			price : +price,
+			discount : +discount,
+			category,
+			description : description.trim(),
+			image : null
+		}
+
+		products.push(product)
+		fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'),JSON.stringify(products,null,30))
+		return res.redirect('/products')
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		return res.render('product-edit-form')
+		const product = products.find(product => product.id === +req.params.id)
+		return res.render('product-edit-form',{
+			...product
+		})
 	},
 	// Update - Method to update
 	update: (req, res) => {
-		// Do the magic
+		const {name,price,description,discount,category} = req.body;
+		const productModify = products.map(product => {
+			
+			if (product.id === +req.params.id) {
+				product.name = name.trim()
+				product.price = +price
+				product.discount = +discount
+				product.category
+				product.description = description.trim()
+			}
+		})
+		fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'),JSON.stringify(productModify,null,30))
+		return res.redirect('/products')
 	},
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
-		// Do the magic
+		const productModify = products.filter(product => product.id !== +req.params.id)
+		fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'),JSON.stringify(productModify,null,30))
+
 	}
 };
 
